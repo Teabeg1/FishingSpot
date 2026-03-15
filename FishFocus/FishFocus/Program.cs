7c0e04d91eb957f226d08fe093beae830ed0c03a
веб-разработчик <kamz4099@gmail.com>
2026-03-16 02:42:41 +0300

    да

--- FishFocus/FishFocus/Program.cs
+++ FishFocus/FishFocus/Program.cs
@@ -5,15 +5,15 @@ using MudBlazor.Services;
 using FishFocus.Services;
 using FishFocus.Interfaces;
 
-builder.Services.AddScoped<FishingService>();
 
-builder.Services.AddScoped<IFishRepository, FakeFishRepository>();
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMudServices();
 builder.Services.AddHttpClient();
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
+builder.Services.AddScoped<FishingService>();
+builder.Services.AddScoped<IFishRepository, FakeFishRepository>();
 
 var app = builder.Build();
 
