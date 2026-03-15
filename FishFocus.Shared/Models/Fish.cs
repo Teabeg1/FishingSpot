7577fb43a17124b0f9832edbe4119b53d920f43e
Elshan <mirz7723@gmail.com>
2026-03-16 01:19:36 +0300

    Добавьте файлы проекта.

---
+++ FishFocus.Shared/Models/Fish.cs
@@ -0,0 +1,8 @@
+﻿namespace FishFocus.Shared.Models;
+
+public class Fish
+{
+    public string Name { get; set; } = "Окунь";
+    public int Rarity { get; set; } // 1 - обычная, 2 - редкая и т.д.
+    public string ImageUrl { get; set; } = "fish_default.png";
+}
---
+++ FishFocus/FishFocus/Program.cs
@@ -0,0 +1,31 @@
+using FishFocus.Client.Pages;
+using FishFocus.Components;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMudServices();
+builder.Services.AddHttpClient();
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(FishFocus.Client._Imports).Assembly);
+app.Run();
