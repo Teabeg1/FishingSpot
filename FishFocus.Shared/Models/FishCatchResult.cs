9d05d012622afc04a604de7b54d870bb88caf700
ponossiedoben <kamz@gmail.com>
2026-03-30 04:46:11 +0300

    очки за рыбу файл

---
+++ FishFocus.Shared/Models/FishCatchResult.cs
@@ -0,0 +1,10 @@
+﻿namespace FishFocus.Shared.Models;
+
+public class FishCatchResult
+{
+    public Fish Fish { get; set; } = new();
+    public int MinutesFished { get; set; }
+    public int BasePoints { get; set; }
+    public int TimeBonus { get; set; }
+    public int TotalPoints { get; set; }
+}
