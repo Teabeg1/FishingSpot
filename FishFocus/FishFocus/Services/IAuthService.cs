258af0ffb43d49e632abf77d7c2dec0296386f5e
ponossiedoben <kamz@gmail.com>
2026-04-01 18:29:54 +0300

    подключены PostgreSQL, JWT и настроен DI в бэкенде

---
+++ FishFocus/FishFocus/Services/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using FishFocus.Shared.DTOs.Auth;
+
+namespace FishFocus.Services;
+
+public interface IAuthService
+{
+    Task<AuthResponse> RegisterAsync(RegisterRequest request);
+    Task<AuthResponse> LoginAsync(LoginRequest request);
+}
