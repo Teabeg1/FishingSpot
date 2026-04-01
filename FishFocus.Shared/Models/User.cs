d5a706ec303dd6b256ce597b1dcd6aef340e3827
ponossiedoben <kamz@gmail.com>
2026-04-01 18:34:11 +0300

    добавлена модель пользователя

---
+++ FishFocus.Shared/Models/User.cs
@@ -0,0 +1,36 @@
+﻿namespace FishFocus.Shared.Models;
+
+public class User
+{
+    //Данные пользователя
+    public int Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public int TotalPoints { get; set; } = 0;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    // Настройки пользователя
+    public bool IsNightMode { get; set; } = false;
+    public bool PlayFog { get; set; } = false;
+
+    // Радио
+    public bool IsRadioEnabled { get; set; } = false;
+    public int RadioVolume { get; set; } = 50;
+
+    // Дождь
+    public bool IsRainEnabled { get; set; } = false;
+    public int RainVolume { get; set; } = 50;
+
+    // Птицы (будущее)
+    public bool IsBirdsEnabled { get; set; } = false;
+    public int BirdsVolume { get; set; } = 50;
+
+    // Волны (будущее)
+    public bool IsWavesEnabled { get; set; } = false;
+    public int WavesVolume { get; set; } = 50;
+
+    // Гром (будущее)
+    public bool IsThunderEnabled { get; set; } = false;
+    public int ThunderVolume { get; set; } = 50;
+}
