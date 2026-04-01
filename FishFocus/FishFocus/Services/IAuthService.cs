f03420f5c9bad3fc435b1c916616997365b7a929
ponossiedoben <kamz@gmail.com>
2026-04-01 18:31:17 +0300

    Revert "подключены PostgreSQL, JWT и настроен DI в бэкенде"
    This reverts commit 258af0ffb43d49e632abf77d7c2dec0296386f5e.

--- FishFocus/FishFocus/Services/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-﻿using FishFocus.Shared.DTOs.Auth;
-
-namespace FishFocus.Services;
-
-public interface IAuthService
-{
-    Task<AuthResponse> RegisterAsync(RegisterRequest request);
-    Task<AuthResponse> LoginAsync(LoginRequest request);
-}
