642e3a53e2a9b536fc377dce425f5457bf2587d7
ponossiedoben <kamz@gmail.com>
2026-04-01 18:30:40 +0300

    подключены PostgreSQL, JWT и настроен DI в бэкенде тоже

--- FishFocus/FishFocus/Program.cs
+++ FishFocus/FishFocus/Program.cs
@@ -1,28 +1,57 @@
-using FishFocus.Repositories; 
-
-using FishFocus.Client.Pages;
-using FishFocus.Components;
-using MudBlazor.Services;
-
+﻿using FishFocus.Components;
+using FishFocus.Data;
 using FishFocus.Services;
 using FishFocus.Interfaces;
 using FishFocus.Repositories;
-
-
+using MudBlazor.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IFishRepository, FakeFishRepository>();
+builder.Services.AddScoped<FishingService>();
+
 builder.Services.AddMudServices();
 builder.Services.AddHttpClient();
-builder.Services.AddRazorComponents()
-    .AddInteractiveWebAssemblyComponents();
-builder.Services.AddScoped<FishingService>();
 builder.Services.AddControllers();
 
-builder.Services.AddScoped<IFishRepository, FakeFishRepository>();
+var jwtKey = builder.Configuration["Jwt:Key"]!;
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
+builder.Services.AddAuthorization();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.UseWebAssemblyDebugging();
 }
 else
@@ -32,11 +61,15 @@ else
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseAntiforgery();
+
 app.MapControllers();
+
 app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(FishFocus.Client._Imports).Assembly);
+
 app.Run();
